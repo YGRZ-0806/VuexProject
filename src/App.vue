7413db5410f288eb7437b92ea969a0ad77f4e456
YGRZ08006 <2223256659@qq.com>
2021-01-14 10:08:31 +0800

    初始化计数器项目的基本结构

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div> 
+    <my-addition></my-addition>
+     <p>----------------------</p>
+     <my-subtraction></my-subtraction>
+  </div>
+</template>
+<script>
+  import Addition from './components/Addition.vue'
+  import Subtraction from './components/Subtraction.vue'
+  export default{
+    components: {
+      //别名
+      'my-addition':Addition,
+      'my-subtraction':Subtraction
+    },
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
