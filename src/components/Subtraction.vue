eeb5a5a81c02c2600dd30532cc7dc3a884d7d48e
YGRZ08006 <2223256659@qq.com>
2021-01-22 16:35:34 +0800

    test

--- src/components/Subtraction.vue
+++ src/components/Subtraction.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h3>当前最新的count值为：</h3>
+    <!-- <h3>当前最新的count值为：</h3> -->
+    <h3>{{ $store.getters.showNum }}</h3>
+    <h3>{{showNum2}}</h3>
+
     <!-- <button @click="sub1">sub1</button>
     <button @click="subN2">subN2</button>
     <button @click="subAsync3">subAsync3</button>
@@ -8,11 +11,15 @@
   </div>
 </template>
 <script>
+  import { mapGetters } from 'vuex'
   export default{
     data () {
       return {
         
       }
+    },
+    computed: {
+      ...mapGetters(['showNum2'])
     }
   }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -27,6 +27,16 @@ export default new Vuex.Store({
       }, 1000);
     }
   },
+  //Getter可以对 Store中已有的数据加工处理之后形成新的数据，类似Vue的计算属性
+  getters: {
+    showNum: state => {
+      return '当前最新的数量是【' + state.count + '】'
+    },
+    showNum2: state => {
+      //return 'Getter可以对 state 中的数据进行加工展示:  ' + state.count +' '
+      return `Getter可以对 state 中的数据进行加工展示:  ${state.count} `
+    }
+  },
   modules: {
   }
 })
